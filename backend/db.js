0e5f09c73b95856aa02199b8082251db0d8244c3
Mani Chaturvedi <manichaturvedi17@gmail.com>
2025-01-20 17:11:35 +0530

    signup signin routes added

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017');
 
-const Userschema=mongoose.Schema({
+const Userschema=new mongoose.Schema({
     username: String,
     password: String,
     firstName: String,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,83 @@
 const express = require('express');
+const zod=require('zod');
+const jwt=require('jsonwebtoken');
+const { User } = require('../db');
+const { JWT_SECRET } = require('../config');
 
 const userrouter = express.Router();
 
-userrouter.post("/signup",)
+const signupbody=zod.object({
+    username:zod.string().email(),
+    firstname:zod.string(),
+    lastname:zod.string(),
+    password:zod.string()
+})
+userrouter.post("/signup",async(req,res)=>{
+    const {success}=signupbody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existinguser=await User.findOne({
+        username:req.body.username
+    })
+
+    if (existinguser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+    const user=await User.create({
+        username:req.body.username,
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        password:req.body.password
+    })
+
+    const userid=user._id;
+
+    const token=jwt.sign({
+        userid
+    },JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+
+
+})
+
+userrouter.post("/signin",async(req,res)=>{
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+    const user=await User.findOne({
+        username:req.body.username,
+        password:req.body.password
+    })
+
+    if(user){
+        const token=jwt.sign({
+            userid:user._id
+        },JWT_SECRET)
+
+        res.json({
+            token:token
+        })
+
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
 
 module.exports = userrouter;
