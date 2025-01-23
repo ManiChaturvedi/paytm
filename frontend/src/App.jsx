e3912ffbd7d61ec5f75398d1a9bb97116eac5bb4
Mani Chaturvedi <manichaturvedi17@gmail.com>
2025-01-23 16:05:14 +0530

    added routes in fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import Dashboard from "./pages/dashboard";
+import SendMoney from "./pages/senmoney";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
-function App() {
-
+  function App() {
   return (
     <div>
-        Hello world
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/send" element={<SendMoney/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
